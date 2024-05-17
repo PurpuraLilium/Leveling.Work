e278080adcf004e2858b98ea5c2da9a0a1079119
Yuri_Gomes <gomesyuridasilva@gmail.com>
2024-05-17 17:24:22 -0300

    feat menu de hamburguer, funcionalidade quase finalizada; funcionalidade formulario finalizada.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// menu hamburguer
+
 let menu = document.getElementById("menu")
 let abrir = document.getElementById("abrir")
 let fechar = document.getElementById("fechar")
-let burguer = document.getElementById("burguer")
+let burger = document.querySelector(".burger")
 
 function AbrirFechar(){
     if(menu.classList.contains("menu-fechado")){
@@ -10,6 +12,9 @@ function AbrirFechar(){
         abrir.style.display = "none"
 
         fechar.style.display = "inline"
+
+        burger.style.width = "250px"
+        
     }
     else{
         menu.classList.add("menu-fechado")
@@ -17,6 +22,9 @@ function AbrirFechar(){
         abrir.style.display = "inline"
 
         fechar.style.display = "none"
+
+        burger.style.width = "50px"
+        
     }
 }
 
@@ -24,4 +32,53 @@ onresize = () => {
     menu.classList.remove("menu-fechado")
     fechar.style.display = "inline"
     abrir.style.display = "none"
+}
+
+// formulario
+
+const formulario = (event) => {
+    // coleta de dados
+
+    let vnome = document.getElementById("name").value
+
+    let vemail = document.getElementById("mail").value
+
+    let vtexto = document.getElementById("txta").value
+
+    // organização de dados
+   
+    let dadosform = {
+        nome: vnome,
+        email: vemail,
+        descricao: vtexto                                                           
+    }
+
+    // enviar para api
+
+    fetch("http://localhost:3000/solicitacao", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dadosform)
+    })
+    .then(resposta => {
+        console.log(resposta)
+
+        // limpar form
+        document.querySelector("#contato form").reset()
+        
+        // aviso de sucesso 
+        alert ("solicitação enviada com sucesso.")
+    })
+
+    .catch(erro => {
+        console.error(erro)
+
+        //  CASO ERRO - mostrar alert
+        alert("Erro na requisição")
+        
+        })
+        
+    event.preventDefault()
 }
